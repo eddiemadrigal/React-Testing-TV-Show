0870082fe399d9b4cebbc868affa49854856ce7f
Eddie Madrigal <edmadrigal@yahoo.com>
2020-03-23 19:15:29 -0700

    files refactored

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,14 +1,9 @@
 import axios from 'axios'
 
-// abstracting
 export const getEpisodes = () => {
     return axios
       .get(
         "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
       )
       .then(res =>  res)
-    
-       /*  setShow(res.data);
-        setSeasons(formatSeasons(res.data._embedded.episodes)); */
-      
 }
